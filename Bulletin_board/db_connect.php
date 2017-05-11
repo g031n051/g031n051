5a78edde8626e2de04ff051adf8cb1f6c83ff6b6
g031n051 <g031n051@s.iwate-pu.ac.jp>
2017-05-11 13:02:36 +0900

    第4回課題

---
+++ Bulletin_board/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$db_user = 'root';     // ユーザー名
+$db_pass = ',Ia+iBips3'; // パスワード
+$db_name = 'bbs';     // データベース名
+
+// MySQLに接続
+$mysqli = new mysqli('localhost', $db_user, $db_pass, $db_name);
+$result = $mysqli->query('select * from `messages`');
+
+// var_dumpで確認
+foreach ($result as $row) {
+  var_dump($row);
+}
